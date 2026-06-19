30b62f3dbda3f97041ab412366f83cfd8d68c8be
kostyamlr-commits <kostyamlr@gmail.com>
2026-06-19 05:14:12 +0300

    add pages/_document.js

---
+++ pages/_document.js
@@ -0,0 +1,12 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+export default function Document() {
+  return (
+    <Html lang="he" dir="rtl">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
