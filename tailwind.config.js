db91d23ac9deba182d6c50619785d359c5f549a3
kostyamlr-commits <kostyamlr@gmail.com>
2026-06-19 05:24:47 +0300

    fix tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
-  theme: {
-    extend: {
-      colors: {
-        pink: { neon: '#ff2d78' },
-        yellow: { neon: '#ffe600' },
-      },
-      fontFamily: {
-        heebo: ['Heebo', 'sans-serif'],
-      },
-    },
-  },
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+  ],
+  theme: { extend: {} },
   plugins: [],
 }
